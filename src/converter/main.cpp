c58eb039f19c57672ff6444cb288a096b569a01a
Marco <marco@marco007.dev>
2026-08-11 20:01:43 +0200

    chore(converter): add C+ Module demo and manifest loading

--- src/converter/main.cpp
+++ src/converter/main.cpp
@@ -1,5 +1,24 @@
+import parser;
+
+#include <yaml-cpp/yaml.h>
 
 int main( ) {
-    std::println( "hello from converter!" );
+    fs::path manifest_path = "data/manifest.yaml";
+    YAML::Node manifest = { };
+    double load_time = 0.0;
+
+    try {
+        std::println( "[?] loading manifest, this might take a second or 2." );
+        auto start = std::chrono::steady_clock::now( );
+        manifest = YAML::LoadFile( manifest_path.string( ) );
+        load_time = std::chrono::duration<double>( std::chrono::steady_clock::now( ) - start ).count( );
+    } catch ( const YAML::BadFile& e ) {
+        throw std::runtime_error( std::format( "[-] failed to open manifest from: {}", manifest_path.string( ) ) );
+    } catch ( const YAML::ParserException& e ) {
+        throw std::runtime_error( std::format( "[-] failed to parse manifest: {}", e.what( ) ) );
+    }
+
+    std::println( "[+] loaded manifest from: {} in {:.2f}s!", manifest_path.string( ), load_time );
+
     return 0;
 }
--- src/pch.hpp
+++ src/pch.hpp
@@ -1,7 +1,15 @@
 #pragma once
+// clang-format off
 
 #if defined( __cplusplus )
     #include <print>
     #include <string>
     #include <vector>
+    #include <filesystem>
+    #include <fstream>
+
+// hax
+namespace fs = std::filesystem;
 #endif
+
+// clang-format on
