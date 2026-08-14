1320806b29c443954398d23221ddbd8c27a1a6c0
Marco <marco@marco007.dev>
2026-08-14 20:32:03 +0200

    fix some shit

--- src/converter/main.cpp
+++ src/converter/main.cpp
@@ -20,9 +20,11 @@ int main( ) {
 
     std::println( "[+] loaded manifest from: {} in {:.2f}s!", manifest_path.string( ), load_time );
 
-    int count = 0;
+    int total_games = 0, games = 0;
     auto find_start = std::chrono::steady_clock::now( );
     for ( const auto& kv : manifest ) {
+        total_games += 1;
+
         // if ( count == 10 ) break; // tmp
 
         std::string name = kv.first.as<std::string>( );
@@ -30,29 +32,31 @@ int main( ) {
 
         if ( !data["files"] ) continue;
 
-        std::println( "{}: {}", count, name );
+        bool kept = false;
+
+        std::println( "Game: {} (#{})", name, games );
         for ( auto&& file : data["files"] ) {
             auto&& [path, entry] = static_cast<std::pair<YAML::Node, YAML::Node>&>( file );
-
-            auto paths = YAML::Dump( path );
-            if ( paths.empty( ) ) continue;
 
             auto tags = YAML::Dump( entry["tags"] );
             if ( tags.contains( "config" ) ) continue;
 
-            count += 1;
+            kept = true;
 
             std::println( "path: {}", YAML::Dump( path ) );
             std::println( "tags: {}", YAML::Dump( entry["tags"] ) );
             std::println( "when: {}", YAML::Dump( entry["when"] ) );
         }
+        if ( !kept ) continue;
         for ( size_t i{ }; i < 20; ++i ) {
             std::print( "-" );
             if ( i == 19 ) std::println( "" );
         }
+        games += 1;
     }
     find_time = std::chrono::duration<double>( std::chrono::steady_clock::now( ) - find_start ).count( );
-    std::println( "[+] found {} games in {:.2f}s", count, find_time );
+    std::println( "[+] found {} games in {:.2f}s", total_games, find_time );
+    std::println( "[-] {}/{} games were invalid entries..", total_games - games, total_games );
 
     return 0;
 }
